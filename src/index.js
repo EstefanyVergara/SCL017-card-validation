6ca9528e1cde32bc87b844189805766d1a0e8e8c
EstefanyVergara <vteffa@gmail.com>
2021-05-23 02:53:02 -0400

    Página de inicio terminada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+function maskify() {
+  let card = document.getElementById("cardNumber").value;
+  card=card.split("");
+  for (i=0; i<card.length; i++){
+    card[i]=parseInt(card[i]);
+   }
+   let hideNum = [];
+   for(let i = 0; i < card.length; i++){
+     if(i < card.length-4){
+       hideNum.push("#");
+    }else{
+       hideNum.push(card[i]);
+     }
+    }
+    document.getElementById("cardNumber").value  = hideNum.join("");
+  }
+
+ 
