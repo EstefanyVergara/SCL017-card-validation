0f99a8583f6fa7c12dfeb55b41fb8e52a9a5d2d9
EstefanyVergara <vteffa@gmail.com>
2021-05-28 20:05:12 -0400

    avances, reparo del código

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,4 @@
-const validator = {
-    // ...
-  };
+function ObtenerNumero()
+    let input = document.getElementById("cardNumber").value;
+    let numbers = input.toString().split("").map(Number)
   
-  export default validator;
