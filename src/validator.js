0d8f68218e2ccf4135815ba4bf4e5f75f4748b4d
EstefanyVergara <vteffa@gmail.com>
2021-05-30 01:28:44 -0400

    Actualización de Readme

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,30 @@
-const validator = {
+const validator = 
+    function isValid(){
+        let card=aux.reverse();
+        for (i=0; i<card.length; i++){
+          card[i]=parseInt(card[i]);
+         }
+        for (j=1; j<card.length; j+=2){
+           card[j]=card[j]*2;
+           card[j]=(card[j]-1)%9+1;
+         }
+          let adition=0;
+          for (i=0; i<card.length; i++){
+            adition=adition+card[i];
+          }
+          let sum=adition;
+          let referencia=sum%10;
+          if(referencia==0){
+              alert("Pago aceptado!!");
+          }else{
+                alert("Tarjeta inválida intente nuevamente");
+                hideNum = [];
+                document.getElementById("textElement").value  = hideNum;
+                aux=[];
+          }
+       }
 
 
-}
    
 
 
