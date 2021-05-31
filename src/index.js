9fc2d271c9e2dd939470bffb06c5bfc7853d3908
EstefanyVergara <vteffa@gmail.com>
2021-05-31 00:43:40 -0400

    Actualización de funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
-
-console.log(validator);
+ 
+let cardNumber=document.getElementById("cardNumber");
+cardNumber.addEventListener("keyup",(e)=>{
+    let creditCardNumber=e.target.value;
+    cardNumber.value=validator.maskify(creditCardNumber);
+});
+ 
+document.getElementById("btn-pay").addEventListener("click",()=>{
+    let creditCardNumber=validator.aux;
+    if(validator.isValid(creditCardNumber)==true){
+       alert("Tarjeta valida");
+    } else {
+       alert("Tarjeta inválida reintente"); 
+    }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,45 @@
-const validator = {
-  // ...
-};
-
-export default validator;
+let aux=[];
+function maskify(tarjeta) {
+    let card = tarjeta;
+    aux.push(card[card.length-1]);
+    card=card.split("");
+    let hideNum = [];
+     for(let i = 0; i < card.length; i++){
+       if(i < card.length-4){
+         hideNum.push("#");
+      }
+      else{
+         hideNum.push(card[i]);
+       }
+      }
+        return hideNum.join("");
+    }
+   
+   function isValid(tarjeta){
+        let booleano="";
+        let card=tarjeta.reverse();
+        
+        for (i=0; i<card.length; i++){
+          card[i]=parseInt(card[i]);
+         }
+        for (j=1; j<card.length; j+=2){
+           card[j]=card[j]*2;
+           card[j]=(card[j]-1)%9+1;
+         }
+          let adition=0;
+          for (i=0; i<card.length; i++){
+            adition=adition+card[i];
+          }
+          let referencia=adition%10;
+          if(referencia==0){
+              booleano=true;
+          }else{
+              booleano=false;            
+          }  
+          return booleano;
+       }
+ 
+  
+      const validator= {aux,maskify,isValid};
+      export default validator;
+      
